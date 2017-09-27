8f62fd14c6e3300f02de7055049d79033a391269
Bruce Spang <bruce@brucespang.com>
2017-09-27 15:53:37 -0700

    histograms, error bars, and multikey hues

--- plorts/plotting.py
+++ plorts/plotting.py
@@ -5,13 +5,13 @@ import numpy as np
 import palettes
 
 def colors_from_hue(data, hue, cmap):
-    num_colors = len(set(data[hue]))
+    num_colors = len(data.groupby(hue))
     cm_subsection = np.linspace(0.2, 0.8, num_colors+2)
     cm_subsection = cm_subsection[1:num_colors+1]
     return [ cmap(v) for v in cm_subsection ]
 
 
-def plot(data, x, y, hue=None, markers=[None], linestyles=['-'], cmap=palettes.neon):
+def plot(data, x, y, error=None, hue=None, markers=[None], linestyles=['-'], cmap=palettes.neon):
     data = data.sort_values(x, ascending=True)
     if hue:
         if cmap is not None:
@@ -24,9 +24,16 @@ def plot(data, x, y, hue=None, markers=[None], linestyles=['-'], cmap=palettes.n
                 color = colors[i]
             else:
                 color = None
-            plt.plot(grp[x], grp[y], label=key, marker=marker, linestyle=linestyle, color=color)
+            if error is not None:
+                plt.errorbar(grp[x], grp[y], yerr=grp[error], label=key, marker=marker, linestyle=linestyle, color=color)
+            else:
+                plt.plot(grp[x], grp[y], label=key, marker=marker, linestyle=linestyle, color=color)
     else:
-        plt.plot(data[x], data[y])
+        color = cmap(0.5)
+        if error is not None:
+            plt.errorbar(data[x], data[y], yerr=data[error], color=color)
+        else:
+            plt.plot(data[x], data[y], color=color)
 
     plt.ylabel(y)
     plt.xlabel(x)
@@ -34,34 +41,49 @@ def plot(data, x, y, hue=None, markers=[None], linestyles=['-'], cmap=palettes.n
     plt.gca().margins(y=0.1)
 
 def stackplot(data, x, y, hue, cmap=palettes.neon):
-    xs = data[x]
+    xs = np.array(data[x])
     yss = []
     labels = []
     for k,grp in data.groupby(hue):
         labels.append(k)
+        grp_xs = grp[x].tolist()
+        grp_ys = grp[y].tolist()
         ys = []
         for v in xs:
-            matching = grp[grp[x] == v]
-            if len(matching) > 0:
-                ys.extend(matching[y])
+            if len(grp_xs) > 0 and grp_xs[0] == v:
+                ys.append(grp_ys.pop(0))
+                grp_xs.pop(0)
             else:
-                ys.append(None)
-        yss.append(ys)
+                if len(ys) == 0:
+                    ys.append(0.)
+                else:
+                    ys.append(ys[-1])
+        assert len(grp_xs) == 0
+        assert len(grp_ys) == 0
+        assert len(ys) == len(xs)
+        yss.append(np.array(ys, dtype=float))
 
     if cmap is not None:
         colors = colors_from_hue(data, hue, cmap)
     else:
         colors = None
 
-    plt.stackplot(xs, yss, labels=labels, colors=colors)
+    plt.stackplot(xs, *yss, labels=labels, colors=colors)
 
     plt.ylabel(y)
     plt.xlabel(x)
     plt.gca().autoscale(tight=True)
     plt.gca().margins(y=0.1)
 
-def scatter(data, x, y, hue=None, markers=[None], cmap=palettes.neon):
+def scatter(data, x, y, hue=None, cmap=palettes.neon):
     return plot(data=data,x=x,y=y, hue=hue,markers=['o'], linestyles=[''],cmap=cmap)
+
+def hist(x, cmap=palettes.neon, **kwargs):
+    if 'color' not in kwargs:
+        kwargs['color'] = cmap(0.5)
+    if 'rwidth' not in kwargs:
+        kwargs['rwidth'] = 0.92
+    plt.hist(x, **kwargs)
 
 def savefig(filename, **kwargs):
     if not os.path.exists(os.path.dirname(filename)):
