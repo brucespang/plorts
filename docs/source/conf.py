c55d466570f5c20557e7040f34b1acc9db22d847
Bruce Spang <bruce@brucespang.com>
2019-07-04 11:56:41 -0700

    correct index file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,6 +28,8 @@ release = open(os.path.join(os.path.dirname(os.path.realpath(__file__)),
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
