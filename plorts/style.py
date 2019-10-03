532da86b815428b7e59f90dd73fd8900d4b080bd
Bruce Spang <bruce@brucespang.com>
2019-10-03 14:33:28 -0700

    improve location of title and yaxis

--- plorts/style.py
+++ plorts/style.py
@@ -34,16 +34,14 @@ import matplotlib.pyplot as plt
 
 # Does a bunch of styling stuff that needs to be done manually instead of in the
 # rcparams
-def style_axis(show_xaxis=False):
+def style_axis(show_xaxis=False, label_fontweight='medium', label_fontstyle='italic'):
     ax = plt.gca()
 
-    ax.xaxis.grid(show_xaxis)
-    ax.title.set_position([-0.04, 1.1])
-    ax.title.set_fontweight("bold")
-    ax.title.set_ha("left")
-    ax.xaxis.label.set_fontweight('medium')
-    ax.xaxis.label.set_fontstyle('italic')
-
+    ax.xaxis.grid(show_xaxis)    
+    ax.xaxis.label.set_fontweight(label_fontweight)
+    ax.xaxis.label.set_fontstyle(label_fontstyle)
+    
+    # position the y label at the top left, left-aligned with the ticks.
     left_in_axis = [0, 1]
     left_in_display = ax.transAxes.transform(left_in_axis)
     left_label_in_display = [left_in_display[0] - ax.yaxis.get_tick_space() * ax.figure.dpi/72.,
@@ -51,9 +49,19 @@ def style_axis(show_xaxis=False):
     left_label_in_axis = ax.transAxes.inverted().transform(left_label_in_display)
 
     ax.yaxis.set_label_coords(*left_label_in_axis)
-
-    ax.yaxis.label.set_fontweight('medium')
-    ax.yaxis.label.set_fontstyle('italic')
-    ax.yaxis.label.set_ha('right')
+    ax.yaxis.label.set_fontweight(label_fontweight)
+    ax.yaxis.label.set_fontstyle(label_fontstyle)
+    ax.yaxis.label.set_ha('left')
+    ax.yaxis.label.set_va('bottom')
     ax.yaxis.label.set_rotation(0)
+
+    # position the y label at the top left, left-aligned with the ticks.
+    title_in_display = [left_in_display[0] - ax.yaxis.get_tick_space() * ax.figure.dpi/72.,
+                        left_label_in_display[1] + ax.xaxis.get_tick_space() * ax.figure.dpi/72.]
+    title_in_axis = ax.transAxes.inverted().transform(title_in_display)
+    ax.title.set_position(title_in_axis)
+    ax.title.set_fontweight("bold")
+    ax.title.set_ha("left")
+    ax.title.set_ha("left")
+    
     plt.tight_layout()
